8dd8befb6f7b81ba91ea2e0a9742bbb2c7a99cb5
Sachin Manohar Khairnar <Sachin.m@Nihon.com>
2026-03-05 11:04:57 +0530

    Add weather API microservice

--- src/main/java/com/example/cicd/controller/HelloController.java
+++ src/main/java/com/example/cicd/controller/HelloController.java
@@ -1,16 +1,34 @@
 package com.example.cicd.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cicd.service.WeatherService;
+
+
 
 @RestController
 public class HelloController {
 	
 	
+	private final WeatherService weatherService;
+	
+	public HelloController(WeatherService weatherService)
+	{
+		this.weatherService=weatherService;
+	}
+	
 	@GetMapping("/")
 	public String hello()
 	{
 		return "Java Microservice running";
+	}
+	
+	@GetMapping("/weather")
+	public String getWeather(@RequestParam double lat,@RequestParam double lon)
+	{
+		return weatherService.getWeather(lat,lon);
 	}
 
 }
